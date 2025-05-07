7d63d424359bf28e6a9d390d7f70dc2aa707f4f8
LorenzoPuppo95 <puppolorenzo@live.it>
2025-05-07 12:43:00 +0200

    commit

--- ClassesAndDataStructures/Program.cs
+++ ClassesAndDataStructures/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace ClassesAndDataStructures
+﻿using System.Collections;
+
+namespace ClassesAndDataStructures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student1 = new Student("pippo", "de pippis", "male", 1980);
+            Student student2 = new Student("paperoga", "de paperoni", 1985);
+            Student student3 = new("paperino", "de paperis", 1985);
+            var student4 = new Student("paperina", "de paperis", 1985);
+            Teacher teacher1 = new Teacher{
+                Name = "mario",
+                Surname = "rossi",
+                Subject = "mathematics"
+            };
+            Teacher teacher2 = new Teacher
+            {
+                Name = "giovanni",
+            };
+
+            Student[] students = new Student[3];
+
+            students[0] = student1;
+            students.Append(student2);
+            students[2] = student3;
+            students.Append(student4);
+
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(student1);
+            arrayList.Add(student2);
+            arrayList.Add(student3);
+            arrayList.Add(student4);
+            arrayList.Add(teacher1);
+            arrayList.Add(teacher2);
+            arrayList.Add("hello");
+            arrayList.Add(1);
+            arrayList.Add(1.5);
+            arrayList.Add(true);
+            arrayList.Add(new DateTime(2023, 10, 1));
+
+            List<Student> studentList = new List<Student>();
+            studentList.Add(student1);
+            studentList.Add(student2);
+            studentList.Add(student3);
+            studentList.Add(student4);
+            //studentList.Add(teacher1); // This will not compile because teacher1 is not a Student
+            //studentList.Add("hello"); // This will not compile because "hello" is not a Student
+
+            Dictionary<string, Student> studentDictionary = new Dictionary<string, Student>();
+            studentDictionary.Add("student1", student1);
+            //studentDictionary.Add(3, student2); // This will not compile because 3 is not a string
+            //studentDictionary.Add("teacher1", teacher1); // This will not compile because teacher1 is not a Student
+
+            SuperList<Student> superList = new SuperList<Student>();
+            superList.Add(student1);
         }
     }
 }
---
+++ ClassesAndDataStructures/SuperList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndDataStructures
+{
+    internal class SuperList<T>
+    {
+        private T[] _realArray;
+
+        public SuperList()
+        {
+            _realArray = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            var originalLength = _realArray.Length;
+            var newArray = new T[originalLength + 1];
+            for (int i = 0; i < originalLength; i++)
+            {
+                newArray[i] = _realArray[i];
+            }
+            newArray[originalLength] = item;
+            _realArray = newArray;
+        }
+    }
+}
